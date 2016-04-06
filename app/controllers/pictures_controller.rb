69120d03a88f1bc17404d990077dd582b93068f1
Vicky L <vicky.sy.loo@gmail.com>
2016-04-06 16:06:55 -0400

    added extra notes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -35,13 +35,17 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
 
     if @picture.update_attributes(picture_params)
-      redirect_to "/pictures/#{@picture.id}"
+      #redirect_to "/pictures/#{@picture.id}"
+      redirect_to picture_path(@picture)
     else
       render :edit
+      #needs to have render (page) or else it would attempt to auto-render back to its method page
+      #which is :update, and that doesn't exist, so it will return an error
     end
   end
 
-  private # anything after this is not going to be processed on front end
+  private # anything after this is not going to be processed on front end, for security reasons
+  # if anyone had access to this they could change your database
 
   def picture_params
     params.require(:picture).permit(:artist, :title, :url)
